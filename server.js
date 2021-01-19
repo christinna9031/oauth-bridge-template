eeadcb4027d94febe45410e5ed3dce47b13c00a8
Christina <73680467+christinna9031@users.noreply.github.com>
2021-01-18 16:06:49 -0800

    Update server.js

--- server.js
+++ server.js
@@ -36,8 +36,9 @@ app.get('/callback', function(req, res) {
   }
   request.post(authOptions, function(error, response, body) {
     var access_token = body.access_token
+    var refresh_token= body.refresh_token
     let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
-    res.redirect(uri + '?access_token=' + access_token)
+    res.redirect(uri + '?access_token=' + access_token +'?refresh_token=' + refresh_token)
   })
 })
 
